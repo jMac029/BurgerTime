8e9039d70310ca43d2fc5d9baf8d0939cc0147a0
James McMillan <jmcmillan29@gmail.com>
2018-01-18 22:22:30 -0800

    fixed some bugs to get the app to work, added in bootstrap for some CSS styling

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
-    database: "burger_db",
+    database: "burgers_db",
     port: 8889
 });
 
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,13 +3,13 @@ const express = require("express")
 const router = express.Router()
 
 // Import the model (cat.js) to use its database functions.
-var burger = require("../models/burger.js");
+let burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", (req, res) => {
-    cat.all(function(data) {
+    burger.all(function(data) {
         let hbsObject = {
-            cats: data
+            burgers: data
         };
         console.log(hbsObject);
         res.render("index", hbsObject);
