5d1b58d81c2b70c9bd7b5fd5c0260b6a6e42b6d8
James McMillan <jmcmillan29@gmail.com>
2018-01-23 21:22:42 -0800

    getting ready to deploy to heroku cleaned up some unneeded files

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+const mysql = require("mysql");
+let connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burgers_db",
-    port: 8889
-});
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "burgers_db",
+        port: 8889
+    });
+}
 // Make connection.
 connection.connect(function(err) {
     if (err) {
